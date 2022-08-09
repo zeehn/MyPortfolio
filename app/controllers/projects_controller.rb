708d145bc596f6c67a366cf4dabbf9b55e318d08
Zakir Hussain <zack.mesacc@gmail.com>
2022-08-08 20:08:35 -0500

    Implement new and create action for projects resource

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,23 @@ class ProjectsController < ApplicationController
   def index 
     @projects = Project.all 
   end
+
+  def new 
+    @project = Project.new 
+  end
+
+  def create 
+    @project = Project.new(project_params)
+
+    if @project.save 
+      redirect_to @project, notice: 'Project successfully created!'
+    else
+      render :new 
+    end
+  end
+
+private 
+  def project_params 
+    params.require(:project).permit(:title, :sub_title, :description, :main_image_file_name, :thumb_image_file_name)
+  end
 end
