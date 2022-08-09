4b2261b30a754a3b3177c84a1c0bcb5f7e7b86b8
Zakir Hussain <zack.mesacc@gmail.com>
2022-08-08 20:44:02 -0500

    Refactor projects resource

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index 
     @projects = Project.all 
   end
@@ -18,16 +20,12 @@ class ProjectsController < ApplicationController
   end
 
   def show 
-    @project = Project.find(params[:id])
   end
 
   def edit 
-    @project = Project.find(params[:id])
   end
   
   def update 
-    @project = Project.find(params[:id])
-
     if @project.update(project_params)
       redirect_to @project, notice: 'Project successfully updated!'
     else 
@@ -36,12 +34,15 @@ class ProjectsController < ApplicationController
   end
 
   def destroy 
-    @project = Project.find(params[:id])
     @project.destroy 
     redirect_to projects_path 
   end
 
 private 
+  def set_project 
+    @project = Project.find(params[:id])
+  end
+
   def project_params 
     params.require(:project).permit(:title, :sub_title, :description, :main_image_file_name, :thumb_image_file_name)
   end
