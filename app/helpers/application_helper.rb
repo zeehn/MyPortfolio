f547b3484b1573c9783d6ca96cb86bba01cc43d2
Zakir Hussain <zack.mesacc@gmail.com>
2022-08-11 18:15:19 -0500

    Style home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def auth_links
+  def auth_links(style)
     if current_user.is_a?(GuestUser) 
-      (link_to "Register", new_user_registration_path) + "</br>".html_safe + (link_to "Login", new_user_session_path)
+      (link_to "Register", new_user_registration_path, class: style) + " " + (link_to "Login", new_user_session_path, class: style)
     else
-      link_to "Logout", destroy_user_session_path, method: :delete 
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end 
   end
 
