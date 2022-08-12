07082ef2155e7eea75d996f01fe8b4eb5af81af5
Zakir Hussain <zack.mesacc@gmail.com>
2022-08-11 20:22:15 -0500

    Style forgot password and change password forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def auth_links(style)
+  def auth_links(style='')
     if current_user.is_a?(GuestUser) 
       (link_to "Register", new_user_registration_path, class: style) + " " + (link_to "Login", new_user_session_path, class: style)
     else
